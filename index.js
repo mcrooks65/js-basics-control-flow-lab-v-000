aca7c05305a223c18b724dce5293d979c398c897
Matthew Crooks <mfcrooks@gmail.com>
2018-08-12 22:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch(tip) {
     case 'generous':
-      return "Thank you so much.";
+      return "Thank you so much."
     case 'not as generous':
-      return "Thank you";
+      return "Thank you"
     default
-      return 'Bye.';
-
+      return 'Bye.'
+    }
   }
 
 
